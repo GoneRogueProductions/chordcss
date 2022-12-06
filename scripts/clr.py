db33dd34c053e8808a9f388caf7ee7ed620271ea
werdl <116349156+werdl@users.noreply.github.com>
2022-12-06 11:14:45 +0000

    Update clr.py

--- scripts/clr.py
+++ scripts/clr.py
@@ -1,1 +1,12 @@
-
+def ret(default,clrs,classmain):
+    lenclr=len(clrs)
+    for x in range(lenclr):
+        print("\n")
+        print("c-"+classmain+"-"+clrs[x])
+        print("{")
+        print(default)
+        print("color: "+clrs[x])
+        print("}")
+#default=preexisting css
+#clrs=list of colors
+#classmain=main color list
